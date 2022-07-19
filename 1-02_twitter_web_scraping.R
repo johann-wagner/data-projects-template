eddf8bfa2c17a61f8ee7b651db8040bca05107ee
johann-wagner <johann.wagner@gmail.com>
2022-07-19 13:08:20 +0000

    Trying to get rtweet to work

--- 1-02_twitter_web_scraping.R
+++ 1-02_twitter_web_scraping.R
@@ -1,11 +1,25 @@
 ### Setup and Configuration ----------------------------------------------------
 source(here::here('0-00_setup_and_configuration.R'))
-library(twitteR)
-
+library(rtweet)
+library(httr)
+library(httpuv)
 
 ### Twitter API Setup ----------------------------------------------------------
 
 source(here::here('api_keys.R'))
 
-consumer_token
-#setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+my_authorization <- create_token(
+  app = "JWDataProjects",
+  consumer_key = consumer_token,
+  consumer_secret = consumer_secret)
+
+
+keith_tweets <- get_timeline(
+  c("helloitskeith"),
+  token = my_authorization)
+
+search_tweets("#helloitskeith", n = 5000, include_rts = FALSE, token = my_authorization)
+test <- search_users("#love", n = 200)
+random_stream <- stream_tweets("", token = my_authorization)
+stream_london <- stream_tweets(location = lookup_coords("london"), timeout = 60)
+
